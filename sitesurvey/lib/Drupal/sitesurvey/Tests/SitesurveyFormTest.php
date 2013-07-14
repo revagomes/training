7195c28c9c40a599cdf2c870e3da56a6dd75ad47
Ken Rickard <agentrickard@gmail.com>
2013-07-14 17:30:40 -0400

    Basic form tests.

--- sitesurvey/lib/Drupal/sitesurvey/Tests/SitesurveyFormTest.php
+++ sitesurvey/lib/Drupal/sitesurvey/Tests/SitesurveyFormTest.php
@@ -32,27 +32,41 @@ class SitesurveyFormTest extends SitesurveyTestBase {
     $items = array(
       'name' => array(
         'label' => 'Your name',
+        'input' => 'Jane Doe',
       ),
       'purpose' => array(
         'label' => 'Why did you visit the site today?',
+        'input' => 'To find a phone number.',
       ),
       'success' => array(
         'label' => 'Did you find what you were looking for?',
+        'input' => '1',
       ),
       'recommend' => array(
         'label' => 'Would you recommend the site to your friends?',
+        'input' => '1',
       ),
       'improve' => array(
         'label' => 'What one thing could we do to improve the site?',
+        'input' => 'Use black text for easier reading.',
       ),
       'hear' => array(
         'label' => 'How did you hear about the site?',
+        'input' => 'Search engine',
       )
     );
     foreach ($items as $key => $value) {
       $this->assertText($value['label'], format_string('Form field label for %field found.', array('%field' => $value['label'])));
       $this->assertRaw("name=\"$key\"", format_string('Form markup for %html field found.', array('%html' => $key)));
+      // Set up edit data.
+      $edit[$key] = $value['input'];
     }
+
+    // Post the form.
+    $this->drupalPost($this->path, $edit, 'Submit');
+
+    // Check the response message.
+    $this->assertText('Thank you!', 'Thank you message shown.');
 
   }
 
