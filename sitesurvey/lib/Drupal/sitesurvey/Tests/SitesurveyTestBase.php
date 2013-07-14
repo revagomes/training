05a32cb011d96935afc53a4a6131b2382d1b1ce0
Ken Rickard <agentrickard@gmail.com>
2013-07-14 16:10:13 -0400

    Fixes line indents.

--- sitesurvey/lib/Drupal/sitesurvey/Tests/SitesurveyTestBase.php
+++ sitesurvey/lib/Drupal/sitesurvey/Tests/SitesurveyTestBase.php
@@ -1,8 +1,8 @@
 <?php
 
 /**
-* @file
-* Definition of Drupal\sitesurvey\Tests\SitesurveyTestBase.
-*/
+ * @file
+ * Definition of Drupal\sitesurvey\Tests\SitesurveyTestBase.
+ */
 
 namespace Drupal\sitesurvey\Tests;
