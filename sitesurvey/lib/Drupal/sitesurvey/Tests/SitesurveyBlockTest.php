e82939bc4007ed15991377bcdf7aa941e51f3128
Ken Rickard <agentrickard@gmail.com>
2013-07-14 17:50:51 -0400

    Initial block tests.

--- sitesurvey/lib/Drupal/sitesurvey/Tests/SitesurveyBlockTest.php
+++ sitesurvey/lib/Drupal/sitesurvey/Tests/SitesurveyBlockTest.php
@@ -22,4 +22,32 @@ class SitesurveyBlockTest extends SitesurveyTestBase {
     );
   }
 
+  /**
+   * Test the form handler.
+   */
+  public function testSitesurveyBlock() {
+    // Let anonymous users view the form.
+    user_role_grant_permissions(DRUPAL_ANONYMOUS_RID, array($this->permission));
+
+    // Place the block.
+    $this->drupalPlaceBlock('sitesurvey_block');
+
+    // Go to the home page.
+    $this->drupalGet('');
+
+    // Look for the block.
+    $this->assertText('Site Survey', 'Site survey block loaded for privileged users.');
+    $this->assertRaw('sitesurvey">Take our site survey</a>', 'Site survey link shown to privileged users.');
+
+    // Take away the permission.
+    user_role_revoke_permissions(DRUPAL_ANONYMOUS_RID, array($this->permission));
+
+    // Go to the home page.
+    $this->drupalGet('');
+
+    // Look for the block.
+    $this->assertNoText('Site Survey', 'Site survey block not shown to unprivileged users.');
+    $this->assertNoRaw('sitesurvey">Take our site survey</a>', 'Site survey link not shown to unprivileged users.');
+
+  }
 }
