28dd1ae6a4700e6ec9c9ec579d370238de85ded2
Ken Rickard <agentrickard@gmail.com>
2013-07-22 18:19:12 -0400

    Working form route.

--- sitesurvey/lib/Drupal/sitesurvey/Form/SitesurveyForm.php
+++ sitesurvey/lib/Drupal/sitesurvey/Form/SitesurveyForm.php
@@ -6,3 +6,41 @@
  */
 
 namespace Drupal\sitesurvey\Form;
+
+use Drupal\Core\Form\FormInterface;
+
+/**
+ * Displays the Site Survey form.
+ */
+class SitesurveyForm implements FormInterface {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormID() {
+    return 'sitesurvey_form';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, array &$form_state) {
+    return $form;
+  }
+
+
+  /**
+   * {@inheritdoc}
+   */
+  public function validateForm(array &$form, array &$form_state) {
+
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, array &$form_state) {
+
+  }
+
+}
