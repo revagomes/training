db7c08eb2efaccf4e2f2f97447ba36152dd1b47b
Ken Rickard <agentrickard@gmail.com>
2013-07-23 10:25:03 -0400

    Functioning form.

--- sitesurvey/lib/Drupal/sitesurvey/Form/SitesurveyForm.php
+++ sitesurvey/lib/Drupal/sitesurvey/Form/SitesurveyForm.php
@@ -25,6 +25,57 @@ class SitesurveyForm implements FormInterface {
    * {@inheritdoc}
    */
   public function buildForm(array $form, array &$form_state) {
+ /* * Name
+  * Why did you visit the site today?
+  * Did you find what you were looking for?
+    * Yes
+    * No
+  * Would you recommend the site to your friends?
+    * Yes
+    * No
+  * What one thing could we do to improve the site?
+  * How did you hear about the site?
+    * Search engine
+    * Friend recommended
+    * Advertisement  */
+    $form['name'] = array(
+      '#type' => 'textfield',
+      '#title' => t('Your name'),
+    );
+    $form['purpose'] = array(
+      '#type' => 'textarea',
+      '#title' => t('Why did you visit the site today?'),
+    );
+    $form['success'] = array(
+      '#type' => 'radios',
+      '#title' => t('Did you find what you were looking for?'),
+      '#options' => array(1 => t('Yes'), 0 => t('No')),
+      '#required' => TRUE,
+    );
+    $form['recommend'] = array(
+      '#type' => 'radios',
+      '#title' => t('Would you recommend the site to your friends?'),
+      '#options' => array(1 => t('Yes'), 0 => t('No')),
+      '#required' => TRUE,
+    );
+    $form['improve'] = array(
+      '#type' => 'textarea',
+      '#title' => t('What one thing could we do to improve the site?'),
+    );
+    $form['hear'] = array(
+      '#type' => 'select',
+      '#title' => t('How did you hear about the site?'),
+      '#options' => drupal_map_assoc(array(
+        'Search engine',
+        'Friend recommended',
+        'Advertisement',
+      )),
+      '#required' => TRUE,
+    );
+    $form['submit'] = array(
+      '#type' => 'submit',
+      '#value' => t('Submit'),
+    );
     return $form;
   }
 
@@ -33,14 +84,14 @@ class SitesurveyForm implements FormInterface {
    * {@inheritdoc}
    */
   public function validateForm(array &$form, array &$form_state) {
-
+    // Deliberately left blank.
   }
 
   /**
    * {@inheritdoc}
    */
   public function submitForm(array &$form, array &$form_state) {
-
+    drupal_set_message(t('Thank you!'));
   }
 
 }
