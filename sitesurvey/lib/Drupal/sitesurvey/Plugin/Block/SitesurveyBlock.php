8e1df7c34068f683d28e5f79e1e8d61c562c1c0b
Ken Rickard <agentrickard@gmail.com>
2013-07-24 18:35:28 -0400

    Block placement and functionality.

--- sitesurvey/lib/Drupal/sitesurvey/Plugin/Block/SitesurveyBlock.php
+++ sitesurvey/lib/Drupal/sitesurvey/Plugin/Block/SitesurveyBlock.php
@@ -6,3 +6,38 @@
  */
 
 namespace Drupal\sitesurvey\Plugin\Block;
+
+use Drupal\block\BlockBase;
+use Drupal\Component\Annotation\Plugin;
+use Drupal\Core\Annotation\Translation;
+
+/**
+ * Provides a Site Survey link block.
+ *
+ * @Plugin(
+ *   id = "sitesurvey_block",
+ *   admin_label = @Translation("Site Survey"),
+ *   module = "sitesurvey"
+ * )
+ */
+class SiteSurveyBlock extends BlockBase {
+
+  /**
+   * Overrides \Drupal\block\BlockBase::access().
+   */
+  public function access() {
+    // @TODO: This construction will go away.
+    // See https://drupal.org/node/2049309
+    return user_access('respond to site survey');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    return array(
+      '#markup' => l(t('Take our site survey'), 'sitesurvey'),
+    );
+  }
+
+}
