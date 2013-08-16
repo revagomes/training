50f7c7817b93bb610888ac0267cb1cce8d918bdc
Ken Rickard <agentrickard@gmail.com>
2013-08-16 08:40:39 -0500

    Adds controller stub.

---
+++ sitesurvey/lib/Drupal/sitesurvey/Controller/SitesurveyController.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @file
+ * Contains \Drupal\sitesurvey\Controller\SitesurveyController.
+ */
+
+namespace Drupal\sitesurvey\Controller;
+
+use Drupal\Core\Controller\ControllerInterface;
+
+/**
+ * Controller routines for domain routes.
+ */
+class DomainController implements ControllerInterface {
+
+}
